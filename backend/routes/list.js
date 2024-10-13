bb2bf933eda77975d5a304be4741cc39771bad5f
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-10-13 21:43:12 +0530

    todayyyy

--- backend/routes/list.js
+++ backend/routes/list.js
@@ -1,62 +1,85 @@
-const router=require('express').Router();
-const User=require('../models/user');
-const List=require('../models/list');
+const router = require('express').Router();
+const User = require('../models/user');
+const List = require('../models/list');
 
+router.post("/addtask", async (req, res) => {
+    try {
+        const { title, body, email } = req.body;
+        const existingUser = await User.findOne({ email });
 
-
-router.post("/addtask",async(req,res)=>{
-    try 
-    {
-    const{title,body,email}=req.body;
-    const existingUser=await User.findOne({email});
-    if(existingUser)
-    {
-        const list=new List({title,body,user:existingUser});
-        await list.save().then(()=>res.status(200).json({list}));
-        existingUser.list.push(list);
-        existingUser.save();
-    } 
-    }
-    catch (error) {
-        console.log(error);
-    }
-})
-
-router.put("/updatetask/:id",async(req,res)=>{
-    try
-    {
-        const{title,body,email}=req.body;
-        const existingUser=await User.findOne({email});
-        if(existingUser)
-        {
-            const list=await List.findByIdAndUpdate(req.params.id,{title,body})
-            list.save().then(()=>res.status(200).json({message:"Task updated"}));
+        if (existingUser) {
+            const list = new List({ title, body, user: existingUser._id }); 
+            await list.save();
+            existingUser.list.push(list._id); 
+            await existingUser.save(); 
+            return res.status(200).json({ list });
+        } else {
+            return res.status(404).json({ message: "User not found" });
         }
-    }
-    catch(err)
-    {
-        console.log(err);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server error" });
     }
 });
 
 
+router.put("/updatetask/:id", async (req, res) => {
+    try {
+        const { title, body, email } = req.body;
+        const existingUser = await User.findOne({ email });
 
-router.delete("/deletetask/:id",async(req,res)=>{
-    try
-    {
-        const {email}=req.body;
-        const existingUser= await User.findOne({email});
-        if(existingUser)
-        {
-            await List.findByIdAndDelete(req.params.id).then(()=>
-            res.status(200).json({message:"Task deleted"}));
-        } 
+        if (existingUser) {
+            const list = await List.findByIdAndUpdate(req.params.id, { title, body }, { new: true });
+
+            if (list) {
+                return res.status(200).json({ message: "Task updated", list });
+            } else {
+                return res.status(404).json({ message: "Task not found" });
+            }
+        } else {
+            return res.status(404).json({ message: "User not found" });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server error" });
     }
-    catch(err)
-    {
-        console.log("error");
+});
+
+router.delete("/deletetask/:id", async (req, res) => {
+    try {
+        const { email } = req.body;
+        const existingUser = await User.findOne({ email });
+
+        if (existingUser) {
+            const list = await List.findByIdAndDelete(req.params.id);
+
+            if (list) {
+                existingUser.list.pull(list._id);
+                await existingUser.save();
+                return res.status(200).json({ message: "Task deleted" });
+            } else {
+                return res.status(404).json({ message: "Task not found" });
+            }
+        } else {
+            return res.status(404).json({ message: "User not found" });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Server error" });
     }
-})
+});
 
 
-module.exports=router;
+router.get("/gettasks/:id",async (req,res)=>{
+   const list=await List.find({user:req.params.id}).sort({createdAt:-1});
+   if(list.length!=0)
+   {
+    res.status(200).json({list:list});
+   }
+   else
+   {
+    res.status(200).json({message:"No Tasks"});
+   }
+});
+
+module.exports = router;
---
+++ frontend/todo/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black text-white'>
+      <p className='text-blue-800'>hllo</p>
+    
+    </div>
+  )
+}
+
+export default App
