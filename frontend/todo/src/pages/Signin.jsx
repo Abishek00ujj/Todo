18380d2caeacbadeea1d6bd9460c39997e1a1143
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-10-17 20:47:22 +0530

    now

--- frontend/todo/src/pages/Signin.jsx
+++ frontend/todo/src/pages/Signin.jsx
@@ -1,8 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
+import {Link} from 'react-router-dom'
 const Signin = () => {
-  return (
+    const emailref=useRef(null);
+    const passwordref=useRef(null);
+    const handleLogin=()=>{
+    }
+  return(
     <>
       <Navbar />
       <div className="w-screen h-screen flex justify-center items-center relative z-10">
@@ -10,19 +15,22 @@ const Signin = () => {
             <p className='text-white font-bold text-2xl'>SIGN IN</p>
           <input
             type="email"
+            ref={emailref}
             required
             className="p-3 border border-slate-500 rounded-lg m-2"
             placeholder="Enter your Email"
           />
           <input
             type="password"
+            ref={passwordref}
             required
             className="p-3 border border-slate-500 rounded-lg m-2"
             placeholder="Password"
           />
-          <button className="p-3 border border-slate-500 rounded-lg m-2 text-white bg-slate-700">
+          <button className="p-3 border border-slate-500 rounded-lg m-2 text-white bg-slate-700" onClick={handleLogin}>
             SIGN IN
           </button>
+          <div className='w-full flex justify-center'><p className='text-white'>Don't have an account?<Link to="/signup" className='text-red-400 underline'> Sign up</Link></p></div>
         </div>
       </div>
       <Footer />
--- frontend/todo/src/pages/Signup.jsx
+++ frontend/todo/src/pages/Signup.jsx
@@ -1,8 +1,16 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
-
+import {Link} from 'react-router-dom'
 const Signup = () => {
+    const nameref=useRef(null);
+    const emailref=useRef(null);
+    const password1ref=useRef(null);
+    const password2ref=useRef(null);
+
+    const handleSignin=()=>{
+        
+    }
   return (
     <>
       <Navbar />
@@ -11,31 +19,36 @@ const Signup = () => {
         <p className='text-white font-bold text-2xl'>SIGN UP</p>
           <input
             type="text"
+            ref={nameref}
             required
             className="p-3 border border-slate-500 rounded-lg m-2"
             placeholder="Enter your Name"
           />
           <input
             type="email"
+            ref={emailref}
             required
             className="p-3 border border-slate-500 rounded-lg m-2"
             placeholder="Enter your Email"
           />
           <input
             type="password"
+            ref={password1ref}
             required
             className="p-3 border border-slate-500 rounded-lg m-2"
             placeholder="Password"
           />
           <input
             type="password"
+            ref={password2ref}
             required
             className="p-3 border border-slate-500 rounded-lg m-2"
             placeholder="Re-Enter Password"
           />
-          <button className="p-3 border border-slate-500 rounded-lg m-2 text-white bg-slate-700">
+          <button className="p-3 border border-slate-500 rounded-lg m-2 text-white bg-slate-700" onClick={handleSignin}>
             SIGN UP
           </button>
+        <div className='w-full flex justify-center'><p className='text-white'>Already have an Account<Link to="/signin" className='text-red-400 underline'> Sign in</Link></p></div>    
         </div>
       </div>
       <Footer />
