d73bbd989cb845959c86fb639dba04894200bbcd
Abishek <151960865+Abishek00ujj@users.noreply.github.com>
2024-10-26 22:20:04 +0530

    fixed issues

--- backend/connection/conn.js
+++ backend/connection/conn.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose');
 
 const conn=async()=>{
     try{
-        const C=await mongoose.connect(import.meta.env.VITE_MONGO_API);
+        const C=await mongoose.connect("mongodb+srv://abi:1234@cluster0.urze4.mongodb.net/");
         console.log("Connected succesfully");
     }
     catch(e)
